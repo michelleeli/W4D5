c7e4460cc320f01d0d16d7ad9f315646393403d8
ashley <ashleykimca@gmail.com>
2023-05-26 14:29:34 -0400

    Added my_transpose and two_sum

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/firstTdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/firstTdd.rb
@@ -20,7 +20,33 @@ class Array
         newarr
     end 
 
+    def two_sum
+        newarr = []
+        self.each_with_index do |ele1, idx1|
+            self.each_with_index do |ele2, idx2|
+                if idx1 < idx2 && ele1 + ele2 == 0
+                    newarr << [idx1, idx2]
+                end
+            end
+        end
+        newarr
+    end
+
+    def my_transpose
+        newarr = []
+        (0...self.length).each do |idx1|
+            subarr = []
+            (0...self.length).each do |idx2|
+                subarr << self[idx2][idx1]
+            end
+            newarr << subarr
+        end
+        newarr
+    end
+
 end 
+
+
 
 ## `two_sum`
 
@@ -34,4 +60,6 @@ end
 # ```ruby
 # [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
 # ```
+
+
 
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/firstTdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/firstTdd_spec.rb
@@ -13,3 +13,29 @@ describe "Array#my_uniq" do
         expect(array.my_uniq).to eq([1,2,3])
     end  
 end 
+
+describe "Array#two_sum" do 
+    let(:array) {[-1, 0, 2, -2, 1]}
+
+    it "returns an array of pairs of positions where the elements at those positions sum to zero" do 
+        expect(array.two_sum).to eq([[0, 4], [2, 3]])
+    end
+
+    it "it returns elements in order of indices" do
+        expect([2, -2, 3, -1, 1].two_sum).to eq([[0,1], [3,4]])
+    end
+end
+
+describe "Array#my_transpose" do 
+    let(:array) {[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ]}
+
+      it "will convert between the row-oriented and column-oriented representations" do 
+        expect(array.my_transpose).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+      end
+
+end
+
